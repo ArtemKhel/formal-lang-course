72fb31633a25d58f9fbc274436a79967be802188
ArtemKhel <ArtemKhel@gmail.com>
2023-09-22 20:11:35 +0300

    Clean-up

--- project/main.py
+++ project/main.py
@@ -3,29 +3,6 @@ from project.utils.rpq import *
 
 
 def main():
-    expected = NondeterministicFiniteAutomaton()
-    expected.add_start_state(0)
-    expected.add_final_state(4)
-    expected.add_transition(0, 'a', 0)
-    expected.add_transition(0, 'a', 1)
-    expected.add_transition(0, 'b', 1)
-    expected.add_transition(0, 'b', 3)
-    expected.add_transition(3, 'c', 4)
-    one = NondeterministicFiniteAutomaton()
-    one.add_start_state(0)
-    one.add_final_state(1)
-    one.add_transition(0, 'a', 0)
-    one.add_transition(0, 'b', 0)
-    one.add_transition(0, 'b', 1)
-    one.add_transition(1, 'c', 1)
-    two = NondeterministicFiniteAutomaton()
-    two.add_start_state(0)
-    two.add_final_state(2)
-    two.add_transition(0, 'a', 0)
-    two.add_transition(0, 'a', 1)
-    two.add_transition(0, 'b', 1)
-    two.add_transition(1, 'c', 2)
-
     s = '(a|b)*.b.c*'
     ss = 'a*.(a|b).c'
     n = regex_to_dfa(s)
@@ -41,17 +18,9 @@ def main():
 
     # inter = intersect_nfa(one, two)
     inter = intersect_nfa(n, nn).to_nfa()
-    res = rpq(one.to_networkx(), ss, {0}, {1})
 
     save_graph_as_dot(inter.to_networkx(), './_inter.dot')
     save_graph_as_dot(inter.minimize().to_networkx(), './_inter_min.dot')
-
-    # assert not inter.accepts(['a'])
-    # assert inter.accepts(['b'])
-    # assert inter.accepts(['a','b'])
-    assert inter.is_equivalent_to(expected)
-    print('YAY')
-
     pass
 
 
