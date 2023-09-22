9481dda8b5d72658abf00d688bf61c99e3bfb4d1
ArtemKhel <ArtemKhel@gmail.com>
2023-09-22 20:11:35 +0300

    Clean-up

--- project/utils/rpq.py
+++ project/utils/rpq.py
@@ -1,6 +1,5 @@
 from typing import Dict, Any
 
-from pyformlang.finite_automaton import EpsilonNFA
 from scipy.sparse import dok_matrix, kron, lil_matrix
 
 from project.utils.automata import *
@@ -12,33 +11,22 @@ class BooleanDecomposition:
     start_states: lil_matrix
     final_states: lil_matrix
     n_states: int
-    states_map: dict[int:int]  # TODO: needed only for init
 
-    def __init__(self, nfa: NondeterministicFiniteAutomaton = None):
-        # nfa = nfa.remove_epsilon_transitions()
+    def __init__(self, nfa: NondeterministicFiniteAutomaton):
         self.matrices = dict()
-        if nfa is not None:
-            nfa = nfa.remove_epsilon_transitions()
+        nfa = nfa.remove_epsilon_transitions()
 
-            self.n_states = len(nfa.states)
-            self.states_map = {s: i for i, s in enumerate(nfa.states)}
+        self.n_states = len(nfa.states)
+        states_map = {s: i for i, s in enumerate(nfa.states)}
 
-            self.start_states = lil_matrix((1, self.n_states), dtype=bool)
-            for s in nfa.start_states:
-                self.start_states[0, self.states_map[s]] = True
+        self.start_states = lil_matrix((1, self.n_states), dtype=bool)
+        for s in nfa.start_states:
+            self.start_states[0, states_map[s]] = True
 
-            self.final_states = lil_matrix((1, self.n_states), dtype=bool)
-            for s in nfa.final_states:
-                self.final_states[0, self.states_map[s]] = True
+        self.final_states = lil_matrix((1, self.n_states), dtype=bool)
+        for s in nfa.final_states:
+            self.final_states[0, states_map[s]] = True
 
-            self.construct_bd_matrix(nfa)
-        else:
-            self.start_states = lil_matrix((1, 0), dtype=bool)
-            self.final_states = dok_matrix((1, 0), dtype=bool)
-            self.n_states = 0
-            self.states_map = dict()
-
-    def construct_bd_matrix(self, nfa: NondeterministicFiniteAutomaton):
         for cur_state, transitions in nfa.to_dict().items():
             for symbol, next_states_set in transitions.items():
                 if symbol not in self.matrices.keys():
@@ -46,20 +34,20 @@ class BooleanDecomposition:
                 if not isinstance(next_states_set, set):
                     next_states_set = {next_states_set}
                 for next_state in next_states_set:
-                    self.matrices[symbol][self.states_map[cur_state], self.states_map[next_state]] = True
+                    self.matrices[symbol][states_map[cur_state], states_map[next_state]] = True
 
     def to_nfa(self) -> NondeterministicFiniteAutomaton:
-        enfa = EpsilonNFA()
+        nfa = NondeterministicFiniteAutomaton()
         for label, matrix in self.matrices.items():
             for (v, w), _ in matrix.items():
-                enfa.add_transition(v, label, w)
+                nfa.add_transition(v, label, w)
 
-        for _i, j in zip(*self.start_states.nonzero()):
-            enfa.add_start_state(j)
-        for _i, j in zip(*self.final_states.nonzero()):
-            enfa.add_final_state(j)
+        for i in self.start_states.nonzero()[1]:
+            nfa.add_start_state(i)
+        for i in self.final_states.nonzero()[1]:
+            nfa.add_final_state(i)
 
-        return enfa.remove_epsilon_transitions()
+        return nfa.remove_epsilon_transitions()
 
     def transitive_closure(self) -> dok_matrix:
         transitions = sum(self.matrices.values())
@@ -87,7 +75,7 @@ def intersect_nfa(
     intersection.start_states = kron(bd_1.start_states, bd_2.start_states)
     intersection.final_states = kron(bd_1.final_states, bd_2.final_states)
 
-    return intersection  # .to_nfa()
+    return intersection
 
 
 def rpq(
@@ -99,8 +87,8 @@ def rpq(
     tc = intersection.transitive_closure()
 
     res = set()
-    for _, start in zip(*intersection.start_states.nonzero()):
-        for _, final in zip(*intersection.final_states.nonzero()):
+    for start in intersection.start_states.nonzero()[1]:
+        for final in intersection.final_states.nonzero()[1]:
             if tc[start, final]:
                 res.add((start, final))
     return res
